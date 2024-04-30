1d53a995195c36371d32155d3b24aad2d47a4273
callmeaf <alikhoshkar2000@gmail.com>
2024-04-30 08:56:16 +0330

    Set name for all routes.

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix(config('callmeaf-base.api.prefix_url'))->as(config('callmeaf-base.api.prefix_route_name'))->middleware(config('callmeaf-base.api.middlewares'))->group(function() {
     Route::middleware(config('callmeaf-media.middlewares.global'))->group(function() {
         Route::prefix('media')->controller(config('callmeaf-media.controllers.media'))->group(function() {
-            Route::delete('/{media}','destroy');
+            Route::delete('/{media}','destroy')->name('media.destroy');
         });
     });
 });
