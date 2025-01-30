2b23b3e8dc079dd2b01e203a68b5e417e323b5ea
callmeaf <alikhoshkar2000@gmail.com>
2025-01-30 15:11:25 +0330

    Change permission destroy method media ( separated from super admin with normal user )

--- src/Utilities/V1/Api/Media/MediaFormRequestAuthorizer.php
+++ src/Utilities/V1/Api/Media/MediaFormRequestAuthorizer.php
@@ -3,12 +3,17 @@
 namespace Callmeaf\Media\Utilities\V1\Api\Media;
 
 use Callmeaf\Base\Utilities\V1\FormRequestAuthorizer;
+use Callmeaf\Permission\Enums\PermissionName;
 
 class MediaFormRequestAuthorizer extends FormRequestAuthorizer
 {
     public function destroy(): bool
     {
+        $authUser = authUser(request: $this->request);
+        if($authUser->isSuperAdminOrAdmin()) {
+            return userCan(PermissionName::MEDIA_DESTROY);
+        }
         $media = $this->request->route('media');
-        return strval($media->model_id) === strval(authUser(request: $this->request)->id);
+        return userCan(PermissionName::MEDIA_DESTROY) && strval($media->model_id) === strval($authUser->id);
     }
 }
