4ac87d53ef7ebc3e63981d695c3562cad52366af
callmeaf <alikhoshkar2000@gmail.com>
2025-02-02 13:21:54 +0330

    Add Product Categories Disk

--- src/Enums/MediaDisk.php
+++ src/Enums/MediaDisk.php
@@ -7,6 +7,7 @@ enum MediaDisk: string
     case UPLOADS = 'uploads';
     case USERS = 'users';
     case PRODUCTS = 'products';
+    case PRODUCT_CATEGORIES = 'product_categories';
     case VARIATIONS = 'variations';
     case PAYMENTS = 'payments';
     case ADDRESSES = 'addresses';
